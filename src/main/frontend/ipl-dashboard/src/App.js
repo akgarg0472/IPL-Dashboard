ed0d6b7778106b7c9f18ad623b7cb5c86aff7728
Akhilesh Garg <akgarg0472@gmail.com>
2021-05-25 12:28:43 +0530

    Updated React App Added MatchPage.js page and modified App.js

--- src/main/frontend/ipl-dashboard/src/App.js
+++ src/main/frontend/ipl-dashboard/src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import { TeamPage } from "./pages/TeamPage";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { MatchPage } from "./pages/MatchPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path='/teams/:teamName'>
-          <TeamPage />
-        </Route>
+        <Switch>
+          <Route path="/teams/:teamName/matches/:year">
+            <MatchPage />
+          </Route>
+
+          <Route path="/teams/:teamName">
+            <TeamPage />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/main/frontend/ipl-dashboard/src/pages/MatchPage.js
@@ -0,0 +1,12 @@
+import { React, useEffect, useState } from "react";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
+import { useParams } from "react-router-dom";
+
+export const MatchPage = () => {
+  return (
+    <div className="MatchPage">
+      <h1>Match page</h1>
+    </div>
+  );
+};
