0f33e1d5f2356383214123bac7cad4fbb0d32326
Akhilesh Garg <akgarg0472@gmail.com>
2021-05-25 13:56:47 +0530

    Added individual team match fetch with year

--- src/main/java/com/akgarg/ipldashboard/controller/TeamController.java
+++ src/main/java/com/akgarg/ipldashboard/controller/TeamController.java
@@ -5,11 +5,9 @@ import com.akgarg.ipldashboard.model.Team;
 import com.akgarg.ipldashboard.repository.MatchRepository;
 import com.akgarg.ipldashboard.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -29,13 +27,37 @@ public class TeamController {
         this.matchRepository = matchRepository;
     }
 
-    @GetMapping("/team/{teamName}")
+
+    @RequestMapping(value = "/team/{teamName}", method = RequestMethod.GET)
     public Team getTeam(@PathVariable("teamName") String teamName) {
         Team team = this.teamRepository.findByTeamNameIgnoreCase(teamName);
         List<Match> matches = this.matchRepository.findLatestMatchesByTeam(teamName, 4);
         team.setMatches(matches);
 
         return team;
+    }
+
+    @RequestMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable("teamName") String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        teamName = getFirstCharacterUpperCaseString(teamName);
+
+        //        return this.matchRepository.getByTeamOneAndDateBetweenOrTeamTwoAndDateBetweenOrderByDateDesc(teamName, startDate, endDate, teamName, startDate, endDate);
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+    }
+
+
+    //helper method to convert first character of each work from lowercase to upper of given string
+    private String getFirstCharacterUpperCaseString(String teamName) {
+        String[] arr = teamName.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : arr) {
+            sb.append(Character.toUpperCase(s.charAt(0)))
+                    .append(s.substring(1)).append(" ");
+        }
+        return sb.toString().trim();
     }
 
 }
--- src/main/java/com/akgarg/ipldashboard/repository/MatchRepository.java
+++ src/main/java/com/akgarg/ipldashboard/repository/MatchRepository.java
@@ -3,8 +3,11 @@ package com.akgarg.ipldashboard.repository;
 import com.akgarg.ipldashboard.model.Match;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -19,4 +22,15 @@ public interface MatchRepository extends CrudRepository<Match, Long> {
         return getMatchesByTeamOneOrTeamTwoIgnoreCaseOrderByDateDesc(teamName, teamName, pageable);
     }
 
+
+    //    List<Match> getByTeamOneAndDateBetweenOrTeamTwoAndDateBetweenOrderByDateDesc(
+    //            String teamOne, LocalDate dateFirst, LocalDate dateSecond,
+    //            String teamTwo, LocalDate dateOne, LocalDate dateTwo
+    //    );
+
+    @Query("select m from Match m where (m.teamOne = :teamName or m.teamTwo = :teamName) and m.date between :startDate and :endDate order by m.date desc")
+    List<Match> getMatchesByTeamBetweenDates(@Param("teamName") String teamName,
+                                             @Param("startDate") LocalDate startDate,
+                                             @Param("endDate") LocalDate endDate
+    );
 }
